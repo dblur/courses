6f51c6e90e177de118de7a6572542389a9ff069a
Dmitriy Kutyavin <69790592+dblur@users.noreply.github.com>
2020-10-18 16:08:05 +0700

    Create step_4.1.08.java

---
+++ stepik/step_4.1.08.java
@@ -0,0 +1,14 @@
+/*
+Реализуйте метод sqrt(), вычисляющий квадратный корень числа. В отличие от Math.sqrt(), 
+это метод при передаче отрицательного параметра должен бросать исключение java.lang.IllegalArgumentException 
+с сообщением "Expected non-negative number, got ?", где вместо вопросика будет подставлено фактически переданное в метод число.
+*/
+
+public static double sqrt(double x) {
+    if (x >= 0){
+    return (double) (Math.sqrt(x));
+    }
+    else {
+        throw new IllegalArgumentException("Expected non-negative number, got " + x);
+        }
+}
